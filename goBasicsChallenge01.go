115b5d3e9d6b84636e2802fb758b634052e74ea6
Clemens Scholz <clemens.scholz@aldi-sued.com>
2022-03-21 10:25:28 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,11 @@
+// Just a basic challenge solution
+
+package main
+  
+import "fmt"
+
+func main() {
+        fmt.Println("Hello, my name is Pirx Danford")
+	fmt.Println("I live in one of the most sunny cities in Germany: Freiburg im Breisgau")
+}
+
